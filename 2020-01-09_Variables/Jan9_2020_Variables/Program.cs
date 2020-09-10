0077c2225629ef8cf1d69df30a4e2ed856e820ca
Ayman Al-Dali <aymand1982@gmail.com>
2020-09-10 00:52:34 -0600

    Add files via upload

---
+++ 2020-01-09_Variables/Jan9_2020_Variables/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace Jan9_2020_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //get first and second number from user & assign to a variable
+            int firstNum = 10, secondNum = 3;
+
+            //calculate sum
+            int sum = firstNum + secondNum;
+            //calculate diff
+            int difference = firstNum - secondNum;
+            //calculate product
+            int product = firstNum * secondNum;
+            //calculate quotient
+            //multiply by 1.0 to convert it into a double that display the decimal values
+            double quotient = firstNum * 1.0 / secondNum;
+
+            //display what the two numbers are
+            Console.WriteLine($"The numbers are {firstNum} and {secondNum}");
+            //display the results sum
+            Console.WriteLine($"The sum is: {sum}");
+            //display the results difference
+            Console.WriteLine($"The difference is: {difference}");
+            //display the results product
+            Console.WriteLine($"The product is: {product}");
+            //display the results quotient
+            Console.WriteLine($"The quotient is: {quotient}");
+            
+        }
+    }
+}
---
+++ 2020-01-10_Math/2020-01-10_Math/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _2020_01_10_Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double quotient = 10 / 3.0;
+            // display the full double digits
+            Console.WriteLine(quotient);
+            // Math.Round gets rid of the decimals
+            Console.WriteLine("My number is {0}", Math.Round(quotient));
+            // Math.Round and specify the number of decimals
+            Console.WriteLine("My number is {0}", Math.Round(quotient, 3));
+
+            // to use "power to" exponents
+            double myNum = Math.Pow(2, 6);
+            // display it
+            Console.WriteLine("My answer is {0}",myNum);
+
+            // to use square root method
+            double newNum = Math.Sqrt(25);
+            Console.WriteLine("The square root is {0}", newNum);
+
+            // for more visit MSDN C# and search
+
+            Console.WriteLine("==================================");
+
+            // different ways to insert variables into strings
+            int firstNum = 5, secondNum = 56, thirdNum = 1;
+            Console.WriteLine("My numbers are " + firstNum + ", " + secondNum + ", and " + thirdNum + ".");
+            Console.WriteLine("My numbers are {0}, {1}, and {2}.", firstNum, secondNum, thirdNum);
+            Console.WriteLine($"My numbers are {firstNum}, {secondNum}, and {thirdNum}.");
+        }
+    }
+}
